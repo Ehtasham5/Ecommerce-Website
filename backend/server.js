89f8e1e467ef4b6ead858ef74a164fc7e6351f57
Ehtasham5 <ehtashamfaryad29@gmail.com>
2026-09-24 18:59:01 +0500

    feat: add root API endpoint to confirm server is working

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,10 @@ app.use("/api/product", productRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/order", orderRouter)
 
+app.get("/" , (req, res) => {
+  res.send("API WORKING")
+})
+
 // Server
 app.listen(port, () => {
   console.log("Server is started on PORT: " + port);
